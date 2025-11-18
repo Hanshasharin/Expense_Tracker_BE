65041be8ef8056ec120d20a07bd2de25f5753b5b
unknown <sharinhansha@gmail.com>
2025-11-18 12:21:13 +0530

    schema error

--- src/models/category.js
+++ src/models/category.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const categorySchema = new mongoose.Schema({
   userId: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
   name: { type: String, required: true },
-  color: { type: String, required: true }
+  color: { type: String, }
 });
 
 module.exports = mongoose.model("Category", categorySchema);
